10c8c29927141a326aefe81fda61a96b1947f7f3
g2mt <pm2mtr@gmail.com>
2026-04-26 09:30:43 +0000

    fix: wrap in unsafe

--- src/fence.rs
+++ src/fence.rs
@@ -21,44 +21,46 @@ impl Fence {
         let width = self.rect.right - self.rect.left;
         let height = self.rect.bottom - self.rect.top;
 
-        let mem_dc = CreateCompatibleDC(hdc);
-        let bitmap = CreateCompatibleBitmap(hdc, width, height);
-        let old_bitmap = SelectObject(mem_dc, bitmap);
+        unsafe {
+            let mem_dc = CreateCompatibleDC(hdc);
+            let bitmap = CreateCompatibleBitmap(hdc, width, height);
+            let old_bitmap = SelectObject(mem_dc, bitmap);
 
-        // Fill with gray (0x808080)
-        let brush = CreateSolidBrush(0x00808080);
-        let rect = RECT {
-            left: 0,
-            top: 0,
-            right: width,
-            bottom: height,
-        };
-        FillRect(mem_dc, &rect, brush);
-        DeleteObject(brush);
+            // Fill with gray (0x808080)
+            let brush = CreateSolidBrush(0x00808080);
+            let rect = RECT {
+                left: 0,
+                top: 0,
+                right: width,
+                bottom: height,
+            };
+            FillRect(mem_dc, &rect, brush);
+            DeleteObject(brush);
 
-        let blend = BLENDFUNCTION {
-            BlendOp: AC_SRC_OVER as u8,
-            BlendFlags: 0,
-            SourceConstantAlpha: 128, // ~50% transparent
-            AlphaFormat: 0,
-        };
+            let blend = BLENDFUNCTION {
+                BlendOp: AC_SRC_OVER as u8,
+                BlendFlags: 0,
+                SourceConstantAlpha: 128, // ~50% transparent
+                AlphaFormat: 0,
+            };
 
-        AlphaBlend(
-            hdc,
-            self.rect.left,
-            self.rect.top,
-            width,
-            height,
-            mem_dc,
-            0,
-            0,
-            width,
-            height,
-            blend,
-        );
+            AlphaBlend(
+                hdc,
+                self.rect.left,
+                self.rect.top,
+                width,
+                height,
+                mem_dc,
+                0,
+                0,
+                width,
+                height,
+                blend,
+            );
 
-        SelectObject(mem_dc, old_bitmap);
-        DeleteObject(bitmap);
-        DeleteDC(mem_dc);
+            SelectObject(mem_dc, old_bitmap);
+            DeleteObject(bitmap);
+            DeleteDC(mem_dc);
+        }
     }
 }
