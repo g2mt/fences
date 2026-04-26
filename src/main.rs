ede8e394e8468639aec780b0c91e1616967ee1ae
g2mt <pm2mtr@gmail.com>
2026-04-26 05:38:43 +0000

    chore: add windows boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    use windows_sys::Win32::Foundation::*;
+    use windows_sys::Win32::System::Threading::*;
+    use windows_sys::Win32::UI::WindowsAndMessaging::*;
+    use windows_sys::core::*;
+
+    unsafe {
+        let event = CreateEventW(std::ptr::null(), 1, 0, std::ptr::null());
+        SetEvent(event);
+        WaitForSingleObject(event, 0);
+        CloseHandle(event);
+
+        MessageBoxA(0 as _, s!("Ansi"), s!("Caption"), MB_OK);
+        MessageBoxW(0 as _, w!("Wide"), w!("Caption"), MB_OK);
+    }
 }
