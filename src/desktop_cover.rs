ab471a31e4b3399f6e9c08dc98d4967dc6c7ae30
g2mt <pm2mtr@gmail.com>
2026-04-27 20:18:50 +0000

    refactor: process hit test immediately on detection in DesktopCover

--- src/desktop_cover.rs
+++ src/desktop_cover.rs
@@ -192,22 +192,14 @@ impl DesktopCover {
         let x = (lparam & 0xFFFF) as i16 as i32;
         let y = ((lparam >> 16) & 0xFFFF) as i16 as i32;
 
-        let mut hit_icon = None;
-        {
-            let inner = self.inner.lock().unwrap();
-            for fence in inner.fences.iter().rev() {
-                if let Some(hit @ HitTest::Icon(_)) = fence.hit_test(x, y) {
-                    hit_icon = Some(hit);
-                    break;
-                }
+        let mut inner = self.inner.lock().unwrap();
+        for fence in inner.fences.iter().rev() {
+            if let Some(hit @ HitTest::Icon(_)) = fence.hit_test(x, y) {
+                inner.hit_type = Some(hit);
+                drop(inner);
+                self.on_command(IDM_RUN_ICON);
+                return 0;
             }
-        }
-
-        if let Some(hit) = hit_icon {
-            let mut inner = self.inner.lock().unwrap();
-            inner.hit_type = Some(hit);
-            drop(inner);
-            self.on_command(IDM_RUN_ICON);
         }
         0
     }
