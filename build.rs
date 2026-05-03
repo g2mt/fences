b623c5fd71d9261e23a3a6b10d8ea6d96d560fc3
g2mt <pm2mtr@gmail.com>
2026-05-03 15:29:43 +0000

    feat: add button creation and Windows manifest support

--- build.rs
+++ build.rs
@@ -2,6 +2,7 @@ fn main() {
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
         let mut res = winresource::WindowsResource::new();
         res.set_icon("branding/logo.ico");
+        res.set_manifest_file("app.manifest");
         res.compile().unwrap();
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,38 @@
-use windows::Win32::Foundation::HWND;
+use windows::core::*;
+use windows::Win32::Foundation::*;
+use windows::Win32::UI::WindowsAndMessaging::*;
 
 #[derive(Copy, Clone)]
 pub(crate) struct HWNDWrapper(pub HWND);
 unsafe impl Send for HWNDWrapper {}
 unsafe impl Sync for HWNDWrapper {}
+
+pub fn create_button(
+    text: &'static str,
+    x: i32,
+    y: i32,
+    width: i32,
+    height: i32,
+    hwndparent: HWND,
+    hmenu: Option<HMENU>,
+    hinstance: HINSTANCE,
+) -> HWND {
+    let text_u16: Vec<u16> = text.encode_utf16().chain(std::iter::once(0)).collect();
+    unsafe {
+        CreateWindowExW(
+            WINDOW_EX_STYLE(0),
+            w!("BUTTON"),
+            PCWSTR(text_u16.as_ptr()),
+            WS_CHILD | WS_VISIBLE | WINDOW_STYLE(BS_PUSHBUTTON as u32),
+            0,
+            0,
+            0,
+            0,
+            Some(hwndparent),
+            hmenu,
+            Some(hinstance.into()),
+            None,
+        )
+        .unwrap()
+    }
+}
